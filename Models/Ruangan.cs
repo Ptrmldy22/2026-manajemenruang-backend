c70fc82b4907da41e2b25c612ccee8776489a4ab
Ptrmldy22 <maulidyap67@gmail.com>
2026-02-14 10:51:25 +0700

    feat: add CRUD Ruangan module

---
+++ Models/Ruangan.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace _2026_manajemenruang_backend.Models
+{
+    public class Ruangan
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string NamaRuangan { get; set; }
+
+        [Required]
+        public string Gedung { get; set; }
+
+        [Range(1, 1000)]
+        public int Kapasitas { get; set; }
+    }
+}
